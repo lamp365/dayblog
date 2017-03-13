14377a7b850bdfc8e7d4437c1a38de263607ea67
kevin.liu <791845283@qq.com>
2017-03-14 01:13:29 +0800

    modify

--- script/common.js
+++ script/common.js
@@ -91,6 +91,101 @@ var changeActMenu = function(num){
 };
 */
 
+var oneFrameStatu = function (){
+    var aFrameName     = $api.domAll('#headerBanner .frame_name');
+    var chooseStyleBtn = $api.byId('chooseStyleBtn');
+
+    var st = $api.getStorage('movieStyle') == '' ? 'show' : $api.getStorage('movieStyle');
+    
+    //修改当前样式class
+     if(st == 'show')
+        chooseStyleBtn.className = 'list';
+     else if(st == 'list')
+        chooseStyleBtn.className = 'show';
+     else{
+        st = 'show';
+        chooseStyleBtn.className = 'list';
+     }
+
+     
+     // api.execScript({
+     //        frameName: 'movie',
+     //        script: 'changeStyle("'+st+'","1")'
+     // });
+
+    $(".frame_name").removeClass('active');
+    $api.addCls(aFrameName[0],'active');
+}
+
+var twoFrameStatu = function (){
+    var aFrameName     = $api.domAll('#headerBanner .frame_name');
+    var chooseStyleBtn = $api.byId('chooseStyleBtn');
+
+    var st = $api.getStorage('cinemaStyle') == '' ? 'cinema_list' : $api.getStorage('cinemaStyle');
+    
+    //修改当前样式class
+     if(st == 'cinema_list')
+        chooseStyleBtn.className = 'cinema';
+     else if(st == 'cinema')
+        chooseStyleBtn.className = 'cinema_list';
+     else{
+        st = 'cinema_list';
+        chooseStyleBtn.className = 'cinema';
+     }
+     
+
+     // api.execScript({
+     //        frameName: 'cinema',
+     //        script: 'changeStyle("'+st+'","2")'
+     // });
+
+    $(".frame_name").removeClass('active');
+    $api.addCls(aFrameName[1],'active');
+}
+
+
+$(document).delegate("#header #chooseStyleBtn",'click',function(){
+    if($(this).hasClass('list')){
+        api.execScript({
+            frameName: 'movie',
+            script: 'changeStyle("list","1")'
+     });
+     $(this).removeClass("list");
+     $(this).addClass('show');
+    }
+
+
+    if($(this).hasClass('show')){
+        alert('aaa');
+        api.execScript({
+            frameName: 'movie',
+            script: 'changeStyle("show","1")'
+     });
+     $(this).removeClass("show");
+     $(this).addClass('list');
+    }
+
+
+     if($(this).hasClass('cinema_list')){
+        api.execScript({
+            frameName: 'cinema',
+            script: 'changeStyle("cinema_list","2")'
+     });
+     $(this).removeClass("cinema_list");
+     $(this).addClass('cinema');
+    }
+
+
+     if($(this).hasClass('cinema')){
+        api.execScript({
+            frameName: 'cinema',
+            script: 'changeStyle("cinema","2")'
+     });
+     $(this).removeClass("cinema");
+     $(this).addClass('cinema_list');
+    }
+})
+
 //某个影片的详情页
 var goDetail = function(json){
     var options = json || {type: '-1'};
